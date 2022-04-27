08a8e2d5c55c8523d20a3753481c212433bb4892
¨Franco <frangarcia93@icloud.com>
2022-04-27 14:27:13 -0300

    Modificaciones comunes

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = express();
 
 const path = require("path");
 
+app.use(express.static('public'));
+
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
 
   res.sendFile(path.resolve("./views/index.html"))
 });
 
+app.get('/login', (req, res) => {
+
+  res.sendFile(path.resolve("./views/login.html"))
+});
+
+app.get('/register', (req, res) => {
+
+  res.sendFile(path.resolve("./views/register.html"))
+});
 
 app.listen(8080, () => {
   console.log('El servidor del proyecto integrador esta corriendo');
