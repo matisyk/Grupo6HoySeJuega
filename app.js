71424239a1fb64fff042fb267bfe2d1ac4f2e442
¨Franco <frangarcia93@icloud.com>
2022-04-27 23:11:27 -0300

    Trabajos en las url de app.js y htmls. tambien se hizo trabajo en el index navbar fija superior

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 app.use(express.static('public'));
 
+const publicPath = path.resolve(__dirname, './public')
+
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
@@ -22,6 +24,22 @@ app.get('/register', (req, res) => {
 
   res.sendFile(path.resolve("./views/register.html"))
 });
+app.get('/sobreNosotros', (req, res) => {
+
+  res.sendFile(path.resolve("./views/sobreNosotros.html"))
+});
+
+app.get('/formularioJugador', (req, res) => {
+
+  res.sendFile(path.resolve("./views/formularioDatosJugador.html"))
+});
+
+app.get('/FormularioCancha', (req, res) => {
+
+  res.sendFile(path.resolve("./views/formularioDatosCancha.html"))
+});
+
+
 
 app.listen(8080, () => {
   console.log('El servidor del proyecto integrador esta corriendo');
