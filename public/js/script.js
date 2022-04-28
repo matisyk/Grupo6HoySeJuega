42c3c1bd98847e7252c454b3d3fc47dc4d293c39
¨Franco <frangarcia93@icloud.com>
2022-04-28 15:37:57 -0300

    Se hizo el slide del banner

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+$('.slideshow').jdSlider({
+
+	wrap:'.slide-inner', //Especificar el slide que vamos a usar
+	isAuto: true, //Inicia la animación automáticamente
+	isLoop: true, //Al finalizar vuelve a comenzar
+	interval: 7000, //Tiempo por cada slide
+	isCursor:true //Pausar animación con el mouse
+
+});
