e8fed4677cc64b6bd933c0fcb544755e02e39b73
matisyk <matisyk@hotmail.com>
2022-05-29 17:31:35 -0300

    multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,7 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
+// const multer = require('multer')
 const path = require ('path')
 
 
