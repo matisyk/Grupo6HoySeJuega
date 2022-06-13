7b80f64815f8331a503d874fd6d0d3f6773db381
matisyk <matisyk@hotmail.com>
2022-06-13 14:30:23 -0300

    form cancha completo

--- src/routes/productsCourtRout.js
+++ src/routes/productsCourtRout.js
@@ -43,7 +43,7 @@ router.post('/registrarCancha', upload.any("img-cancha"), productsController.sto
 // /*** EDIT ONE PRODUCT ***/ 
 
 router.get('/editCourt/:id/', productsController.edit);
-router.patch('/editOwnerForm/:id/',upload.any(), productsController.update); 
+router.patch('/editCourt/:id/',upload.any(), productsController.update); 
 
 
 // /*** DELETE ONE PRODUCT***/ 
