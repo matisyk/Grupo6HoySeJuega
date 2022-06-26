0838274c1aab932cfc2f4acdad1637dd057a4347
¨Franco <frangarcia93@icloud.com>
2022-06-26 19:50:21 -0300

    session pruebas

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,23 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const {
-  validationResult
+  check,
+  validationResult,
+  body
 } = require('express-validator');
-const User = require('../models/User')
 const bcryptjs = require('bcryptjs');
+const usersFilePath = path.join(__dirname, '../database/userOwner.json');
 
 const controllerLogin = {
 
   loginCourt: (req, res) => {
-
-
     res.render("partial/login/loginCourt")
   },
+
   processLogin: (req, res) => {
-    let errors = validationResult(req);
-    if (errors.isEmpty()) {
-      let usersJSON = fs.readFileSync('userOwner.json', {
-        encoding: 'utf8'
+    const resultValidation = validationResult(req);
+    if (resultValidation.isEmpty()) {
+      let usersJSON = fs.readFileSync(usersFilePath, {
+        encoding: 'utf-8'
       });
       let users;
       if (usersJSON == "") {
@@ -25,32 +26,29 @@ const controllerLogin = {
       } else {
         users = JSON.parse(usersJSON)
       }
+      
+      let emailUsuario;
+      let contrasenaUsuario;
 
       for (let i = 0; i < users.length; i++) {
         if (users[i].email == req.body.email) {
-          if (bcryptjs.compareSync(req.body.password, users[i].password)) {
-            let usuarioALoguearse = users[i];
-            break;
-          }
-        };
+        emailUsuario = users[i].email
+        
+        }
+        if (users[i].password == req.body.password) {
+          contrasenaUsuario = users[i].password
+        }
+        if (emailUsuario == users[i].email && contrasenaUsuario == users[i].password) {
+           res.send('pasaste bien');
+       } else {
+           res.send('no podes pasar')
       }
-      if (usuarioALoguearse == undefined) {
-        return res.render('loginCourt', {
-          errors: [
-            {msg: 'Credenciales invalidas'}
-          ]
-        });
-      } 
-      req.session.usuarioALogueado = usuarioALoguearse;
-      res.render("partial/register/redireccion", {
-        id
-      })
-
+      }
+   
     } else {
-      return res.render('loginCourt', {
-        errors: errors
-      });
+      return res.send('algo mal hiciste Rick');
     }
+
   },
 
   loginPlayer: (req, res) => {
--- src/routes/loginRout.js
+++ src/routes/loginRout.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router();
-const {check} = require('express-validator');
+const {body} = require('express-validator');
+
 // const multer = require('multer');
 
-
+const validations = [
+  body("email").notEmpty().withMessage('Tienes que escribir un email'),
+];
 
 const loginController = require('../controllers/loginController')
 
 router.get('/loginCourt', loginController.loginCourt);
-router.post('/loginCourt',[check('email').isEmail().withMessage('Este email no es valido')], loginController.processLogin);
+router.post('/loginCourt',validations, loginController.processLogin);
 router.get('/loginPlayer', loginController.loginPlayer);
 
 module.exports = router;
