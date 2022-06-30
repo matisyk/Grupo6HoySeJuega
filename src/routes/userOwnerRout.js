96847ed9bc62640da92cf055f36a9cef61ccad89
¨Franco <frangarcia93@icloud.com>
2022-06-30 19:38:26 -0300

    Se acomoda lo prohibir acceso si no sos el usuario correcto

--- src/routes/userOwnerRout.js
+++ src/routes/userOwnerRout.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router();
 const noLogueadoOwner = require('../middlewares/noLogueadoOwner');
+const logueadoPlayer = require('../middlewares/logueadoPlayer')
 // const multer = require('multer');
 
 
 
 const userOwnerController = require('../controllers/userOwnerController')
 
-router.get('/agenda',noLogueadoOwner, userOwnerController.agenda);
-router.get('/crearTorneo',noLogueadoOwner, userOwnerController.crearTorneo);
-router.get('/registrarCancha',noLogueadoOwner, userOwnerController.registrarCancha);
-router.get('/registrarEscuelita',noLogueadoOwner, userOwnerController.registrarEscuelita);
-router.get('/reservaCancha',noLogueadoOwner, userOwnerController.reservaCancha);
-router.get('/vistaCancha/:id',   userOwnerController.vistaCancha);
-router.get('/vistaCancha/:id', noLogueadoOwner,  userOwnerController.canchas);
+router.get('/agenda',noLogueadoOwner, logueadoPlayer,userOwnerController.agenda);
+router.get('/crearTorneo',noLogueadoOwner,logueadoPlayer, userOwnerController.crearTorneo);
+router.get('/registrarCancha',noLogueadoOwner, logueadoPlayer,userOwnerController.registrarCancha);
+router.get('/registrarEscuelita',noLogueadoOwner, logueadoPlayer,userOwnerController.registrarEscuelita);
+router.get('/reservaCancha',noLogueadoOwner, logueadoPlayer,userOwnerController.reservaCancha);
+router.get('/vistaCancha/:id',  logueadoPlayer, userOwnerController.vistaCancha);
+router.get('/vistaCancha/:id', noLogueadoOwner, logueadoPlayer, userOwnerController.canchas);
 router.post('/vistaCancha', userOwnerController.vistaCancha);
 
 
--- src/routes/userPlayerRout.js
+++ src/routes/userPlayerRout.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-
+const logueadoOwner = require('../middlewares/logueadoOwner')
 const noLogueadoPlayer = require('../middlewares/noLogueadoPlayer')
 
 // const multer = require('multer');
@@ -9,20 +9,20 @@ const noLogueadoPlayer = require('../middlewares/noLogueadoPlayer')
 
 const userPlayerController = require('../controllers/userPlayerController')
 
-router.get('/',noLogueadoPlayer, userPlayerController.vistaJugador);
+router.get('/',noLogueadoPlayer,  logueadoOwner,userPlayerController.vistaJugador);
 
-router.get('/perfilDeJugador/:id', noLogueadoPlayer, userPlayerController.perfilDeJugador);
-router.post('/perfilDeJugador',  userPlayerController.perfilDeJugador);
+router.get('/perfilDeJugador/:id', logueadoOwner, noLogueadoPlayer, userPlayerController.perfilDeJugador);
+router.post('/perfilDeJugador', logueadoOwner,  userPlayerController.perfilDeJugador);
 
 router.get('/carrito', userPlayerController.carrito);
 
 router.get('/reservarCancha', userPlayerController.reservarCancha);
 
-router.get('/vistaCanchaInfo/:id',  noLogueadoPlayer, userPlayerController.vistaCanchaInfo);
+router.get('/vistaCanchaInfo/:id',  noLogueadoPlayer, logueadoOwner, userPlayerController.vistaCanchaInfo);
 router.post('/vistaCanchaInfo', userPlayerController.vistaCanchaInfo);
 
-router.get('/elegirCancha', userPlayerController.elegirCancha);
+router.get('/elegirCancha', logueadoOwner, userPlayerController.elegirCancha);
 
-router.get('/equipo',noLogueadoPlayer, userPlayerController.equipo);
+router.get('/equipo',noLogueadoPlayer, logueadoOwner, userPlayerController.equipo);
 
 module.exports = router;
