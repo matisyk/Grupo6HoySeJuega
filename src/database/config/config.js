bbefc67019bbff15c59ce57f8563cfde515a0004
¨Franco <frangarcia93@icloud.com>
2022-07-31 08:54:40 -0300

    pequenas modificaciones de css

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,15 +1,15 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "hoy_se_juega",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "database_test",
+    "password": "root",
+    "database": "hoy_se_juega",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
